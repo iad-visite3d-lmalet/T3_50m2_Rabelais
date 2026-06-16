ded7db8fe4b19d51c9e116f672bd620f555dce0a
lmalet-dot <lmalet@gmail.com>
2026-06-16 10:21:55 +0200

    main

---
+++ data.js
@@ -0,0 +1,307 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-palier",
+      "name": "palier",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -1.8103506120686994,
+        "pitch": 0.3067589147237033,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.547739509700527,
+          "pitch": 0.3168550628830502,
+          "rotation": 0,
+          "target": "1-couloir"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-couloir",
+      "name": "couloir",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 2.1601831580753235,
+        "pitch": 0.19123589427693233,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6592488569387083,
+          "pitch": 0.23429386523021734,
+          "rotation": 0,
+          "target": "2-salon"
+        },
+        {
+          "yaw": 3.019247410534737,
+          "pitch": 0.19331538069616983,
+          "rotation": 0,
+          "target": "4-ch1"
+        },
+        {
+          "yaw": -1.5887692968117175,
+          "pitch": 0.1443157207780672,
+          "rotation": 0,
+          "target": "5-ch2"
+        },
+        {
+          "yaw": -1.7640946548435252,
+          "pitch": 0.15003137839347858,
+          "rotation": 0,
+          "target": "6-sdb"
+        },
+        {
+          "yaw": 0.5681967491068942,
+          "pitch": 0.11165278018266278,
+          "rotation": 0,
+          "target": "0-palier"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salon",
+      "name": "salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.5782644393077305,
+        "pitch": 0.11946288473481737,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14802782386566982,
+          "pitch": 0.1950896290078017,
+          "rotation": 0,
+          "target": "3-balcon"
+        },
+        {
+          "yaw": 2.336896749230206,
+          "pitch": 0.1688568337552887,
+          "rotation": 0,
+          "target": "1-couloir"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-balcon",
+      "name": "balcon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -2.148282400022845,
+        "pitch": 0.018863289814978756,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8444144264979343,
+          "pitch": 0.24077018794287497,
+          "rotation": 0,
+          "target": "2-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-ch1",
+      "name": "ch1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -1.7376196348308852,
+        "pitch": 0.2993240700452997,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.693454981571156,
+          "pitch": 0.18458657628696784,
+          "rotation": 0,
+          "target": "1-couloir"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-ch2",
+      "name": "ch2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -1.102122278142982,
+        "pitch": 0.456893138108601,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3658247888905697,
+          "pitch": 0.23160152202761353,
+          "rotation": 0,
+          "target": "1-couloir"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sdb",
+      "name": "sdb",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 2.551168462308212,
+        "pitch": 0.34650064503140854,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.606384081354058,
+          "pitch": 0.20720490903620004,
+          "rotation": 0,
+          "target": "1-couloir"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "T3_50m2_Rabelais",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
